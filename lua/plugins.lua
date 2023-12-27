d4b98dd0707a25a2dfc0bd5641097b60cbdc11c0
Nihal Khatwani <nkhatwani24@gmail.com>
2023-12-27 22:22:41 +0400

    chore: easier updating and maintaining plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,4 +12,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("nihal.plugins")
+require("lazy").setup({ { import = "nihal.plugins" }, { import = "nihal.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
